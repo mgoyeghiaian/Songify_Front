6243de1cd772fa46a6981b6bb61b6058f4b2eeff
mgoyeghiaian <mgoyeghiaian@gmail.com>
2023-06-04 19:43:35 +0300

    Fixed some css

--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import './index.css';
-// import App from './App';
+import App from './App';
 import { store } from './redux/store';
-import Apptest from './Apptest';
+// import Apptest from './Apptest';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <Apptest />
-        {/* <App /> */}
+        {/* <Apptest /> */}
+        <App />
       </Router>
     </Provider>
   </React.StrictMode>,
